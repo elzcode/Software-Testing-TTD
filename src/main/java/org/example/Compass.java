1cdb25d7f8429521d8daad34f36eb6b886cad98a
completely_lost <686-completely_lost@users.noreply.gitlab.aicrowd.com>
2024-04-30 16:48:38 +0100

    end of day

---
+++ src/main/java/org/example/Compass.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Compass {
+    public enum Point {
+        NORTH, EAST, SOUTH, WEST
+    }
+
+    public enum Direction {
+        LEFT, RIGHT
+    }
+
+    private Point point;
+
+    public static Point rotate(Point point, Direction direction) {
+        int movement;
+        int index;
+        Point[] points = Point.values();
+
+        if (direction == Direction.RIGHT) {
+            index = (point.ordinal() + 1) % points.length;
+        }else {
+            index = (point.ordinal() - 1 + points.length) % points.length;
+        }
+
+
+
+
+//        // NORTH, EAST, SOUTH, WEST
+//
+//        // forward
+//        index = (point.ordinal() + movement) % points.length;
+//
+//        // move backward
+//        index = (point.ordinal() - movement + points.length) % points.length;
+
+
+        return points[index];
+    }
+}
